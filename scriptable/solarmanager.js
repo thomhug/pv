367b8fa29d623b7192772004125e407cf3797242
thomhug <tom@anbeda.ch>
2023-04-18 00:07:37 +0200

    Update solarmanager.js

--- scriptable/solarmanager.js
+++ scriptable/solarmanager.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: orange; icon-glyph: magic;
+// icon-color: yellow; icon-glyph: magic;
 
 const config = importModule("config")
 const auth = config.auth
