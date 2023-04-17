606702dc4d9c975f6101c05bc5315bf19994d423
thomhug <tom@anbeda.ch>
2023-04-18 00:17:57 +0200

    Update config.js

--- scriptable/config.js
+++ scriptable/config.js
@@ -3,3 +3,16 @@ module.exports = {
   auth: "Basic xxx",
   smid: "xxx"
 }
+
+// Ausgabe der erfolgreichen Speicherung
+let widget = new ListWidget();
+
+widget.addText("In diesem Script befinden sich die Credentials für deinen SolarManager sowie deine Solarmanager ID.");
+
+if (config.runsInWidget) {
+  // Platzieren Sie das Widget auf dem Startbildschirm des iPhones
+  Script.setWidget(widget);
+} else {
+  // Vorschau anzeigen
+  widget.presentSmall();
+}
