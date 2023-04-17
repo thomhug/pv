c00a2aba5e9576a17cff1c0eaa311b562b0d545a
thomhug <tom@anbeda.ch>
2023-04-18 00:18:46 +0200

    Update update_sm_script.js

--- scriptable/update_sm_script.js
+++ scriptable/update_sm_script.js
@@ -1,3 +1,7 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: orange; icon-glyph: magic;
+
 // GitHub-URL des Skripts
 let scriptURL = "https://github.com/thomhug/pv/raw/main/scriptable/solarmanager.js"
 
@@ -5,10 +9,26 @@ let scriptURL = "https://github.com/thomhug/pv/raw/main/scriptable/solarmanager.
 let request = new Request(scriptURL)
 let script = await request.loadString()
 
-// Speichern des Skripts lokal
-let fm = FileManager.local()
-let path = fm.joinPath(fm.documentsDirectory(), "Solarmanager.js")
+// Speichern des Skripts in den Ordner für Scriptable-Skripte
+let fm = FileManager.iCloud()
+let path = fm.joinPath(fm.documentsDirectory(), "")
+if (!fm.isDirectory(path)) {
+  fm.createDirectory(path)
+}
+path = fm.joinPath(path, "Solarmanager.js")
 fm.writeString(path, script)
 
 // Ausgabe der erfolgreichen Speicherung
+let widget = new ListWidget();
+
+widget.addText("Das Skript wurde erfolgreich heruntergeladen und gespeichert.");
+
+if (config.runsInWidget) {
+  // Platzieren Sie das Widget auf dem Startbildschirm des iPhones
+  Script.setWidget(widget);
+} else {
+  // Vorschau anzeigen
+  widget.presentSmall();
+}
+
 console.log("Das Skript wurde erfolgreich heruntergeladen und lokal gespeichert.")
