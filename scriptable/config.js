1abd70146f1a1c76169898feabc41326b5690ffe
thomhug <tom@anbeda.ch>
2023-04-18 07:41:14 +0200

    Update config.js

--- scriptable/config.js
+++ scriptable/config.js
@@ -1,4 +1,6 @@
 // In diesem Script befinden sich die Credentials für deinen SolarManager sowie deine Solarmanager ID.
+// Die Solarmanger ID (smid) findest du in der App
+// Das Auth Token erhältst du, wenn du dich hier: https://external-web.solar-manager.ch/swagger einloggst.
 module.exports = {
   auth: "Basic xxx",
   smid: "xxx"
